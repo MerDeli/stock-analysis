a27486a295153bceffbb75adcce9caf552e13786
lyy <1406557342@qq.com>
2023-03-31 14:26:31 +0800

    【feat】: 安全模块添加

---
+++ stock-security/src/main/java/com/lyy/stock/auth/StockAuthApplication.java
@@ -0,0 +1,13 @@
+package com.lyy.stock.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockAuthApplication.class, args);
+    }
+
+}
